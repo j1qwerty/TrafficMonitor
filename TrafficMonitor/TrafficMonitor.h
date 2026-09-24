9b6e75c6223a475f1fc8fb84007faca9858fcdb2
Saturday <a17tiwari@gmail.com>
2026-09-24 16:47:28 +0530

    Prepare taskbar-only controller and monitor worker

--- TrafficMonitor/TrafficMonitor.h
+++ TrafficMonitor/TrafficMonitor.h
@@ -188,6 +188,9 @@ private:
 
     bool m_is_windows11_taskbar{ false };  //是否为Windows11的任务栏
     COLORREF m_theme_color{};
+#ifdef TASKBAR_ONLY
+    CTrafficMonitorDlg* m_taskbar_only_controller{};
+#endif
 
 // 重写
 public:
